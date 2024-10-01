28ae9be8121ce49606be83cb0a648bd79f230d8a
EnderMelech <ethanjudahw@gmail.com>
2024-10-01 17:07:39 -0400

    idk help

--- ButtonRGB/src/main.cpp
+++ ButtonRGB/src/main.cpp
@@ -1,9 +1,5 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
-#define LED_PIN 48
-#define LED_COUNT 1
-#define BRIGHTNESS 255
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   
