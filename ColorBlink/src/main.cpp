8c407c1ce1497ea80274c1a32a177108b40bbae5
EnderMelech <ethanjudahw@gmail.com>
2024-10-01 12:37:16 -0400

    workingrgbBlink

--- ColorBlink/src/main.cpp
+++ ColorBlink/src/main.cpp
@@ -2,22 +2,25 @@
 #include <Adafruit_NeoPixel.h>
 #define LED_PIN 48
 #define LED_COUNT 1
-#define BRIGHTNESS 50
+#define BRIGHTNESS 255
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void Red () {
-  strip.setPixelColor(strip.Color(255,   0,   0)     , BRIGHTNESS);
+  strip.setPixelColor(0, strip.Color(255,   0,   0));
     strip.show();
+    delay(100);
 }
 
 void Green () {
-  strip.setPixelColor(strip.Color(0,   255,   0)     , BRIGHTNESS);
+  strip.setPixelColor(0, strip.Color(0,   255,   0));
     strip.show();
+    delay(100);
 }
 
 void Blue () {
-  strip.setPixelColor(strip.Color(0,   0,   255)     , BRIGHTNESS);
+  strip.setPixelColor(0, strip.Color(0,   0,   255));
     strip.show();
+    delay(100);
 }
 
 void setup() {
@@ -27,7 +30,7 @@ void setup() {
 }
 
 void loop() {
-  Red;
-  Green;
-  Blue;
+  Red();
+  Green();
+  Blue();
 }
