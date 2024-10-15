75bd2c9a0a6fda822d5085b7aed0e4ab5f8f6159
EnderMelech <ethanjudahw@gmail.com>
2024-10-15 11:41:09 -0400

    idk

--- ColorJoystick/src/main.cpp
+++ ColorJoystick/src/main.cpp
@@ -1,18 +1,66 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+const int xPin = 27;
+const int yPin = 26;
+const int swPin = 25;
+#define LED_PIN 48
+#define LED_COUNT 1
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-// put function declarations here:
-int myFunction(int, int);
+boolean oldState = HIGH;
+int     mode     = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  strip.begin();
+  strip.show();
+}
+
+void Red () {
+  strip.setPixelColor(0, strip.Color(255,   0,   0));
+  strip.show();
+}
+
+void Green () {
+  strip.setPixelColor(0, strip.Color(0,   255,   0));
+  strip.show();
+}
+
+void Blue () {
+  strip.setPixelColor(0, strip.Color(0,   0,   255));
+  strip.show();
+}
+
+void Black() {
+  strip.setPixelColor(0, strip.Color(0,   0,    0));
+  strip.show();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  boolean newState = digitalRead(BUTTON_PIN);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if((newState == LOW) && (oldState == HIGH)) {
+    delay(20);
+    newState = digitalRead(BUTTON_PIN);
+    if(newState == LOW) {
+      if(++mode > 3) mode = 0;
+      switch(mode) {
+        case 0:
+          Black();
+          break;
+        case 1:
+          Red();
+          break;
+        case 2:
+          Green();
+          break;
+        case 3:
+          Blue();
+          break;
+      }
+    }
+  }
+
+  oldState = newState;
 }
