1d95bb1073a92a089e25423bae1d1fad6e50bf58
EnderMelech <ethanjudahw@gmail.com>
2024-12-05 11:52:16 -0500

    Done

--- Ultrasonic Sensor/src/main.cpp
+++ Ultrasonic Sensor/src/main.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
+#include <TimerOne.h>
 #define echo 3
 #define trig 2
 
-int t0=0;
 unsigned long t1=0;
 unsigned long t2=0;
 unsigned long t1Local=0;
 unsigned long t2Local=0;
 bool falling = false;
+
+void triggerSensor() {
+ digitalWrite(trig, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(trig, LOW);
+}
 
 void measureTime() {
  if (digitalRead(echo) == HIGH) {
@@ -22,22 +28,18 @@ void measureTime() {
 void setup() {
  pinMode(trig, OUTPUT);
  pinMode(echo, INPUT);
+ Timer1.initialize(200000);
+ Timer1.attachInterrupt(triggerSensor);
  attachInterrupt(digitalPinToInterrupt(echo), measureTime, CHANGE);
  Serial.begin(9600);
 }
 
 void loop() {
- if (millis()-t0>=100) {
-    t0=millis(); // the time of the most recent spike (when you pulled the trigger)
-    digitalWrite(trig, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trig, LOW);
  if (falling == true) {
    noInterrupts();
    unsigned long t1Local = t1;
    unsigned long t2Local = t2;
    interrupts();
    Serial.println(0.0343*((t2Local-t1Local)/2));
- }
  }
 }
