d5348f55e185afbf22bbf8492afcfcb5335d104c
EnderMelech <ethanjudahw@gmail.com>
2024-12-10 12:08:01 -0500

    Did it?

--- Button Interrupt/src/main.cpp
+++ Button Interrupt/src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 #define button 2
 
+bool oldState = HIGH;
+int mode = 0;
+
+void changeLights() {
+  if
+}
+
 void setup() {
   pinMode(button, INPUT_PULLUP);
-  
+  pinMode(LED_BUILTIN, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(button), changeLights, FALLING);
+  Serial.begin
 }
 
 void loop() {
