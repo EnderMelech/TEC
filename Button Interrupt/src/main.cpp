d3e33f1a174eedd497197673ff82cdeea45d0eb0
EnderMelech <ethanjudahw@gmail.com>
2024-12-12 12:31:31 -0500

    Adding timer to fix extra presses

--- Button Interrupt/src/main.cpp
+++ Button Interrupt/src/main.cpp
@@ -1,19 +1,31 @@
 #include <Arduino.h>
+#include <TimerOne.h>
 #define button 2
 
-bool state = HIGH;
+bool newState = HIGH;
+bool oldState = newState;
+int number = 0;
+
+void checkButton() {
+  if
+}
 
 void changeLights() {
-  digitalWrite(LED_BUILTIN, state);
-  state = !state;
+  digitalWrite(LED_BUILTIN, newState);
+  newState = !newState;
+  number = number + 1;
+  Serial.println(number);
+  Serial.println("Falling");
 }
 
 void setup() {
   pinMode(button, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, state);
-  state = !state;
-  attachInterrupt(digitalPinToInterrupt(button), changeLights, FALLING);
+  digitalWrite(LED_BUILTIN, newState);
+  newState = !newState;
+  Timer1.initialize(30000);
+  Timer1.attachInterrupt(checkButton);
+  //attachInterrupt(digitalPinToInterrupt(button), changeLights, FALLING);
   Serial.begin(9600);
 }
 
