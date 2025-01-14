7f7e2ae25a7790e83b4ec794642f558be60c3e3d
EnderMelech <ethanjudahw@gmail.com>
2025-01-14 11:57:22 -0500

    setting up to work with mqtt

--- ESP32 Relay/src/main.cpp
+++ ESP32 Relay/src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <PubSubClient.h>
+
+IPAddress ip(10, 2, 7, 85);
+IPAddress server(3, 211, 61, 173);
 
 void setup() {
   WiFi.mode(WIFI_STA);
-  WiFi.begin("NETGEAR23-5G","rustictree335");
+  WiFi.begin("NETGEAR23","rustictree335");
   Serial.begin(115200);
   Serial.println("Hello World!");
   while (WiFi.status() != WL_CONNECTED) {
