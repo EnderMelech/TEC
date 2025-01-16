0e04401131135ed0c6c153b400d0358faf2cd0cc
EnderMelech <ethanjudahw@gmail.com>
2025-01-16 15:17:54 -0500

    Working on connecting to nodered

--- ESP32 Relay/src/main.cpp
+++ ESP32 Relay/src/main.cpp
@@ -2,24 +2,91 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-IPAddress ip(10, 2, 7, 85);
-IPAddress server(3, 211, 61, 173);
+const char* ssid = "NETGEAR23";
+const char* password = "rustictree335";
 
-void setup() {
+const char* mqtt_server = "3.211.61.173";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+const int ledPin = 4;
+
+void setup_wifi() {
+  delay(10);
+  Serial.println("");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
   WiFi.mode(WIFI_STA);
-  WiFi.begin("NETGEAR23","rustictree335");
-  Serial.begin(115200);
-  Serial.println("Hello World!");
+  WiFi.begin(ssid, password);
+  
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.println(WiFi.status());
-    delay(100);
+    delay(500);
+    Serial.println(".");
   }
-  WiFi.status();
-  Serial.println(WiFi.status());
-  WiFi.localIP();
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
-void loop() {
+void callback (char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
   
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  if (String(topic) == "esp32/ethan") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+      digitalWrite(ledPin, HIGH);
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP8266Client", "nironif", "qcnfnded666")) {
+      Serial.println("connected");
+      client.subscribe("esp32/ethan");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+client.loop();
 }
